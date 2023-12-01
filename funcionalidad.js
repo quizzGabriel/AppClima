2f8334d1cf43355164bb9ff2d1cdaa631b5c04ae
Jesus <jesus.lopezJPZ@outlook.com>
2023-11-30 20:27:20 -0600

    Funcionalidad-actualizado

--- funcionalidad.js
+++ funcionalidad.js
@@ -1,7 +1,7 @@
 var ciudadeshtml = document.querySelector("#ciudades");
 var paiseshtml = document.querySelector("#Paises");
 var boton = document.getElementById("GetWeathe");
-var imprimeresultado =;
+var imprimeresultado =document.getElementById("resultado");
 
 // console.log(ciudadeshtml.value);
 
@@ -18,17 +18,27 @@ function imprime() {
   // Espera la promesa y obtén los datos
   .then(function(datos) {
     // Muestra algunos datos de los datos
-    console.log("Temperatura: " + datos.main.temp + " K");
+    var tem = parseInt(datos.main.temp);
+    tem = tem - 273.15;  
+    console.log("Temperatura: " + tem + " C");
     console.log("Humedad: " + datos.main.humidity + " %");
     console.log("Descripción: " + datos.weather[0].description);
+    imprimeresultado.innerHTML = `<div class="row text-white">
+                                  <h6>Clima en ${ciudadeshtml.value}</h6> 
+  
+                                 </div>
+                                <div class="row text-center text-white">
+                                <i class="bi bi-cloud-fill" style="font-size: 3em;"></i>
+                                  </div>
+                                <div class="row text-white">
+                                <h1>Temperatura: ${tem} C</h1>
+                                </div>
+                                <div class="row text-white">Humedad:${datos.main.humidity} %</div>
+                                <div class="row text-white">12.27°C</div>`;
   })
   // Captura cualquier error que ocurra
   .catch(function(error) {
     console.error(error);
   });
-}
-
-function imprimeValores(){
 
 }
-
